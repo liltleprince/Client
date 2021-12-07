d751666bb4e398737fb5ac25d4646a5a66cb101c
Tuấn Đào <daotuan1903@gmail.com>
2021-12-07 22:52:46 +0700

    Add data class

--- src/main/java/com/example/client/Client.java
+++ src/main/java/com/example/client/Client.java
@@ -1,4 +1,220 @@
 package com.example.client;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Client {
+    public final static String SERVER_IP = "127.0.0.1";
+    public final static int SERVER_PORT = 8000;
+    public final static int BUFFER_SIZE = 1024;
+    public final static byte[] BUFFER = new byte[BUFFER_SIZE];
+    public static Socket socket;
+    public static BufferedReader is;
+    public static BufferedWriter os;
+    public static Data data;
+    public static DataInputStream recvFile;
+    public static long len;
+    public static String line;
+    boolean connecting = false;
+
+    public Client(){
+        data = Data.getData();
+    }
+
+    public void Connect() {
+        if (!connecting) System.out.println("Connecting to Server...");
+        try {
+            connecting = true;
+            socket = new Socket(SERVER_IP, SERVER_PORT);
+            System.out.println("Connected: " + socket);
+            is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            os = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            recvFile = new DataInputStream(socket.getInputStream());
+            data.setConnected();
+            data.setSocket(socket);
+            connecting = false;
+            //HELO Broker
+            HELOBroker();
+        } catch (IOException e) {
+            data.setDisConnect();
+        }
+    }
+
+    void HELOBroker(){
+        sendMessage("CLIENT HELO Broker");
+        readMessage("200 Helo Client");
+    }
+
+    public void Login(){
+        sendMessage("SIGN IN");
+        if (!readMessage("220 Sign in OK")) return;
+
+        sendMessage("ACCOUNT");
+        if (!readMessage("221 Account OK")) return;
+
+        //send Account
+        //readMessage("222 Login Success");
+    }
+
+    public void CreateAccount(){
+        sendMessage("SIGN UP");
+        if (!readMessage("210 Sign up OK")) return;
+
+        sendMessage("ACCOUNT");
+        if (!readMessage("211 Account OK")) return;
+
+        //send account
+        //readMessage("212 Register Success");
+    }
+
+    public void Logout(){
+        sendMessage("SIGN OUT");
+
+        readMessage("230 Sign out OK");
+    }
+
+    public void getLocation(){
+        sendMessage("GET LOCATION");
+
+        if(!readMessage("240 GET Location OK")) return;
+
+        readFile("location.json");
+    }
+
+    public void getSensor(){
+        sendMessage("GET TYPE");
+
+        if(!readMessage("241 GET Type Sensor OK")) return;
+
+        readFile("sensor.json");
+    }
+
+    public void getSensorRegister(){
+        sendMessage("GET INFO REGISTER");
+
+        if(!readMessage("242 Info Register OK")) return;
+
+        //send locationID
+
+        readFile("sensorRegister.json");
+    }
+
+    public void addSensor(){
+        sendMessage("ADD REGISTER");
+
+        if(!readMessage("243 Add register OK")) return;
+
+        //send sensorid
+    }
+
+    public void removeSensor(){
+        sendMessage("DELETE REGISTER");
+
+        if(!readMessage("244 Delete register OK")) return;
+
+        //send sensorid
+    }
+
+    public void getInfoSensor(){
+        sendMessage("GET INFO SENSOR");
+
+        if(!readMessage("251 Get Info Sensor OK")) return;
+
+        //send locationId
+
+        readMessage("");
+        //readFile("infoSensor.json");
+    }
+
+    public long readFileSize(){
+        try {
+            socket.setSoTimeout(3000);
+            int n = recvFile.read(BUFFER,0,BUFFER_SIZE);
+            if (n == -1){
+                data.setDisConnect();
+                System.out.println("DisConnect to Broker");
+                data.setSucceed(false);
+                return -1;
+            }
+            byte[] bytes = new byte[n];
+            System.arraycopy(BUFFER, 0, bytes, 0, n);
+            System.out.println("Broker: "+ new String(bytes));
+            return 1;
+        } catch (IOException e) {
+            System.out.println("Time out connect to Server");
+            return -1;
+        }
+    }
+
+    public void readFile(String filename) {
+        try {
+            long fileSize = readFileSize();
+            if (fileSize == -1) return;
+            len = 0;
+            File file = new File(filename);
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            while (len < fileSize){
+                socket.setSoTimeout(3000);
+                int n = recvFile.read(BUFFER,0,BUFFER_SIZE);
+                fileOutputStream.write(BUFFER,0,n);
+                len += n;
+            }
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((line = bufferedReader.readLine()) != null){
+                stringBuilder.append(line);
+            }
+            data.setSucceed(true);
+        } catch (IOException e){
+            data.setSucceed(false);
+            System.out.println("Error");
+        }
+    }
+
+    public boolean readMessage(String message){
+        try {
+            socket.setSoTimeout(3000);
+            int n = recvFile.read(BUFFER,0,BUFFER_SIZE);
+            if (n == -1){
+                data.setDisConnect();
+                System.out.println("DisConnect to Broker");
+                data.setSucceed(false);
+                return false;
+            }
+            BUFFER[n] = '\0';
+            byte[] bytes = new byte[n];
+            System.arraycopy(BUFFER, 0, bytes, 0, n);
+            System.out.println("Broker: "+ new String(bytes));
+
+            if (message.equals(new String(bytes))){
+                data.setSucceed(true);
+                return true;
+            }
+
+            data.setSucceed(false);
+            return false;
+        } catch (IOException e) {
+            System.out.println("Time out connect to Server");
+            return false;
+        }
+    }
+
+    public void sendMessage(String message){
+        char[] chars = message.toCharArray();
+        try {
+            os.write(chars,0,chars.length);
+            os.flush();
+            System.out.println("Send to Broker: "+message);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Can't connect to Broker");
+        }
+    }
+
+    public void quit(){
+        sendMessage("QUIT");
+
+        readMessage("999 bye");
+    }
+
 }
--- src/main/java/com/example/client/Data.java
+++ src/main/java/com/example/client/Data.java
@@ -1,10 +1,76 @@
 package com.example.client;
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.json.*;
+
+import java.net.Socket;
+
 public class Data {
     private static final Data data = new Data();
+    public boolean Connected = false, DisConnect = true, Succeed = false;
+    public static Client client = new Client();
+    public Scene Login;
+    public Stage stage;
+    public Socket socket;
+
+
     private Data(){}
 
     public static Data getData(){
         return data;
+    }
+
+    public void setSucceed(boolean succeed){
+        this.Succeed = succeed;
+    }
+
+    public void setConnected(){
+        this.Connected = true;
+        this.DisConnect = false;
+    }
+
+    public void setDisConnect(){
+        this.DisConnect = true;
+        this.Connected = false;
+    }
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void Connect(){
+        client.Connect();
+    }
+
+    public void setLogin(Scene scene){
+        this.Login = scene;
+    }
+
+    public void setSocket(Socket socket){
+        this.socket = socket;
+    }
+
+    public void Login(){
+        client.Login();
+    }
+
+    public void CreateAccount(){
+        client.CreateAccount();
+    }
+
+    public void getLocation(){
+        client.getLocation();
+    }
+
+    public void getSensor(){
+        client.getSensor();
+    }
+
+    public void Logout(){
+        client.Logout();
+    }
+
+    public void quit(){
+        client.quit();
     }
 }
