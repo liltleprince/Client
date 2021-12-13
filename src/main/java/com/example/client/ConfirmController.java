52341118d050e903a5bef2dd1f9d363ce73fe116
Tuấn Đào <daotuan1903@gmail.com>
2021-12-13 10:25:31 +0700

    Class confirm

--- src/main/java/com/example/client/ConfirmController.java
+++ src/main/java/com/example/client/ConfirmController.java
@@ -1,4 +1,44 @@
 package com.example.client;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.stage.Stage;
+
 public class ConfirmController {
+
+    @FXML
+    public Button Cancel;
+    @FXML
+    public Button Ok;
+
+    public void onMouseClickedCancel() {
+        Stage stage = (Stage) Cancel.getScene().getWindow();
+        stage.close();
+    }
+
+    public void onMouseClickedOk() {
+        Stage stage = (Stage) Ok.getScene().getWindow();
+        stage.close();
+        Data data = Data.getData();
+        data.Logout();
+        data.stage.setTitle("Đăng nhập");
+        data.stage.setScene(data.Login);
+    }
+
+    public void onMouseExitedCancel() {
+        Cancel.setEffect(null);
+    }
+
+    public void onMouseExitedOk() {
+        Ok.setEffect(null);
+    }
+
+    public void onMouseMovedCancel() {
+        Cancel.setEffect(new DropShadow());
+    }
+
+    public void onMouseMovedOk() {
+        Ok.setEffect(new DropShadow());
+    }
 }
